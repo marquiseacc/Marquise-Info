b8ff77514873b9a50acec3fade50394da18392eb
marquiseacc <marquiseacc.info@gmail.com>
2025-04-22 17:11:30 +0330

    update4.22.2025.2

---
+++ Marquise_Web.UI/areas/CRM/Controllers/AccountManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marquise_Web.UI.areas.CRM.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        // GET: CRM/AccountManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Marquise_Web.UI/areas/CRM/Controllers/InvoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marquise_Web.UI.areas.CRM.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: CRM/Invoice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
